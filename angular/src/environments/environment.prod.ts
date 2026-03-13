97cdca57c9d2dd4fff5149e0c6d43aa584ef32c7
Nothing <andy.v.centeno@gmail.com>
2026-03-12 21:57:32 -0500

    chore: se eliminaron los archivos de entorno del seguimiento

--- angular/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.miapp.com',
-  supabaseUrl: 'https://smqftlzucmjucqzyorid.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtcWZ0bHp1Y21qdWNxenlvcmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MTAyMjgsImV4cCI6MjA3ODA4NjIyOH0.Yi-abXn-A-6bbzfkJPcnHP1F6OUTwBx3F6i7bk6ucMw'
-};
